c7da6b8d4165855ba711c9323dc415a6ccbbad9b
lalwr <lalwrj@gmail.com>
2018-12-01 18:36:46 +0900

    mybatis test

--- src/main/java/nextstep/dao/UserDTO.java
+++ src/main/java/nextstep/dao/UserDTO.java
@@ -1,8 +1,8 @@
-package nextstep.domain;
+package nextstep.dao;
 
 import nextstep.CannotDeleteException;
 import nextstep.UnAuthorizedException;
-import nextstep.view.QuestionView;
+import nextstep.domain.*;
 import support.domain.AbstractEntity;
 import support.domain.UrlGeneratable;
 
@@ -11,131 +11,35 @@ import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Entity
-public class Question extends AbstractEntity implements UrlGeneratable {
+public class UserDTO {
 
-    @Size(min = 3, max = 100)
-    @Column(length = 100, nullable = false)
-    private String title;
+    private String id;
 
-    @Size(min = 3)
-    @Lob
-    private String contents;
+    private String name;
 
-    @ManyToOne
-    @JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
-    private User writer;
+    private String writer;
 
-    @Embedded
-    private Answers answers;
-
-    private boolean deleted = false;
-
-    public Question() {
+    public String getId() {
+        return id;
     }
 
-    public Question(Long id ,String title, String contents, User writer, Answers answers) {
-        super(id);
-        this.title = title;
-        this.contents = contents;
-        this.writer = writer;
-        this.answers = answers;
+    public String getName() {
+        return name;
     }
 
-    public Question(String title, String contents) {
-        this.title = title;
-        this.contents = contents;
-        this.answers = new Answers();
-    }
-
-    public Question(long id, String title, String contents, User writer) {
-        this(id, title, contents, writer, new Answers());
-    }
-
-    public void writeBy(User loginUser) {
-        this.writer = loginUser;
-    }
-
-    public boolean isOwner(User loginUser) {
-        return writer.equals(loginUser);
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
-    }
-
-    @Override
-    public String generateUrl() {
-        return String.format("/questions/%d", getId());
-    }
-
-    @Override
-    public String toString() {
-        return "Question{" +
-                "title='" + title + '\'' +
-                ", contents='" + contents + '\'' +
-                ", writer=" + writer +
-                ", answers=" + answers +
-                ", deleted=" + deleted +
-                '}';
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
-
-    public User getWriter() {
+    public String getWriter() {
         return writer;
     }
 
-    public String getContents() {
-        return contents;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Answers getAnswers() {
-        return answers;
-    }
-
-    public void addAnswer(Answer answer) {
-        answer.toQuestion(this);
-        answers.addAnswer(answer);
-    }
-
-    public Question update(Question updatedQuestion) {
-        if(!isOwner(updatedQuestion.getWriter())){
-            throw new UnAuthorizedException();
+    public UserDTO(String id, String name) {
+        if(id.length() > 5 || name.length() < 4){
+            throw new IllegalArgumentException();
         }
-
-        this.title = updatedQuestion.title;
-        this.contents = updatedQuestion.contents;
-
-        return this;
     }
 
-    public List<DeleteHistory> delete(User loginUser) throws CannotDeleteException {
-        if(isDeleted()){
-            throw new CannotDeleteException("삭제된 질문입니다.");
-        }
-        if(!isOwner(loginUser)){
-            throw new UnAuthorizedException("작성자가 아닙니다.");
-        }
-        deleted = true;
-        List<DeleteHistory> deleteHistories = answers.delete(loginUser);
-        deleteHistories.add(new DeleteHistory(ContentType.ANSWER, getId(), loginUser, LocalDateTime.now()));
-
-        return deleteHistories;
+    public boolean isOwner(String loginUser) {
+        return writer.equals(loginUser);
     }
+
 
 }
--- src/main/java/nextstep/dao/UserDao.java
+++ src/main/java/nextstep/dao/UserDao.java
@@ -1,4 +1,25 @@
 package nextstep.dao;
 
+import java.util.List;
+
 public class UserDao {
+
+    private SqlSession session;
+
+    public void addUser(UserDTO userDTO){
+        int count = session.select("dao.user.selectUserCnt", userDTO);
+        if(count > 0){
+            throw new IllegalArgumentException();
+        }
+        session.insert("dao.user.addUser", userDTO);
+    }
+
+    public void updateUser(UserDTO userDTO){
+        UserDTO user = session.select("dao.user.selectUser", userDTO);
+        user.isOwner(userDTO.getId());
+
+        UserDTO updateUser = new UserDTO(id, name);
+        session.update("dao.user.updateUser", updateUser);
+    }
+
 }
